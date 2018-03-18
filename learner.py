e3673c192f793cbae5f31b82f884feb79f9e3ca5
Harshika <hrshika.arya@gmail.com>
2018-03-18 20:07:25 +0530

    Decision Tree classification Model added

--- learner.py
+++ learner.py
@@ -11,10 +11,10 @@ import itertools
 from nltk.corpus import stopwords
 nltk.download('stopwords')
 from nltk.stem.porter import PorterStemmer
-
+from nltk.stem import WordNetLemmatizer
 #importing the dataset
 
-path1 = 'C50train/'
+path1 = '/media/harshika/Work/Projects/Author-Identifier/datasets/C50train/'
 authors = os.listdir(path1)[:50]
 
 y_train = []
@@ -40,7 +40,7 @@ for auth in authors:
         X_train = np.append(X_train,cleaned)
         #X_train = ' '.join(X_train)
         f.close()
-path2 = 'C50test/'
+path2 = '/media/harshika/Work/Projects/Author-Identifier/datasets/C50test/'
 authors = os.listdir(path2)[:50]
 for auth in authors:  
 
@@ -54,29 +54,38 @@ for auth in authors:
          cleaned = cleaned.lower()
          #removing stop words using nltk library
          cleaned = cleaned.split()
+         #Lemmatization
+         wordnet_lemmatizer = WordNetLemmatizer()
+         cleaned = [wordnet_lemmatizer.lemmatize(word) for word in cleaned if not word in set(stopwords.words('english'))]
          # Stemming
-         ps = PorterStemmer()
-         cleaned = [ps.stem(word) for word in cleaned if not word in set(stopwords.words('english'))]
+         """ps = PorterStemmer()
+         cleaned = [ps.stem(word) for word in cleaned if not word in set(stopwords.words('english'))]"""
          cleaned = ' '.join(cleaned)
          X_train = np.append(X_train,cleaned)
          f.close()
  # Creating the Bag of Words Model
+ 
 from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer()        
 
 X = cv.fit_transform(X_train).toarray()
-#np.concatenate(y_train)
-#list(sum(y_train, )
 y = y_train
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
 
+"""
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
+
 # Fitting Naive Bayes classifier to the Training set
-#from sklearn.naive_bayes import GaussianNB
-#classifier = GaussianNB()
-#classifier.fit(X_train, y_train)
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
 
 #Fitting SVM Classifier to the training set
 from sklearn.svm import SVC
@@ -88,16 +97,19 @@ from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression()
 classifier.fit(X_train, y_train)
 
+#Fitting Decision Tree Classification to the training set
+from sklearn.tree import DecisionTreeClassifier
+classifier  = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+classifier.fit(X_train, y_train)
 
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
+# Predicting the Test set results 
+y_pred = classifier.predict(X_train)
 
 # Making the confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
-accuracy = np.trace(cm)
-print ((2500-accuracy)/25)
+#calculating accuracy
+from sklearn.metrics import accuracy_score
+accuracy_score(y_test,y_pred)
 
-#visualizing training set results
-from matplotlib.colors import ListedColorMaps 
